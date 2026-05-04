1ad328d0aab2a27c3c491d5a15842b6fffd31a52
Elijah Drakeford <elijahdrakeford12@gmail.com>
2026-05-03 23:06:40 -0400

    init

---
+++ Lab15_edrakeford-1.py
@@ -0,0 +1,13 @@
+"""
+Program Name: Lab15_edrakeford-1.py
+
+Author: Elijah Drakeford
+
+Purpose: This program uses matplotlib to plot a rose curve 
+and displays the plot in an image file
+
+Starter Code: None
+
+Date: May 3, 2026
+"""
+
